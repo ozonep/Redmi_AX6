1f384384b8ca7476bc828704379b022de0b2a38e
Ivan Malkov <ozonep@yandex.ru>
2021-09-07 07:21:50 +0300

    refactor: 💡 slightly updated function

--- reducer.mjs
+++ reducer.mjs
@@ -1,15 +1,8 @@
 import { readFileSync, writeFileSync } from 'fs';
 
-
-
-
 const configReducer = (fileName) => {
     const dataArr = readFileSync(fileName, 'utf8').split('\n');
-    const filteredData = dataArr.filter((line) => {
-        if (line.startsWith('#')) return false;
-        if (line.length === 0) return false;
-        return true;
-    }).sort();
+    const filteredData = dataArr.filter((line) => !(line.startsWith('#') || line.length === 0)).sort();
     writeFileSync(`${fileName}slim`,  filteredData.join('\n'), 'utf8');
 }
 
